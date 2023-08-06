0d85804aefcc2b29534aadce7d30421ed2fe80de
Oleksandr Plokhykh <plohishi.family@gmail.com>
2023-08-06 19:00:02 +0200

    Formik validation + Error message

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import styles from './SignUpFormStyle.module.css';
+import React from 'react';
 import { SCHEMA } from '../../schemas';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 
 const initialState = {
@@ -10,51 +10,34 @@ const initialState = {
     lastName: ''
 }
 
-class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...initialState,
-            error: null
-        }
+function SignUpForm(props) {
+
+    const handleSubmitToFormik = (value, actions) => {
+        console.log(value);
+        actions.resetForm();
     }
 
-    changeHandler = ({ target: { value, name } }) => {
-        this.setState({
-            [name]: value
-        })
-    }
 
-    submitHandler = (event) => {
-        event.preventDefault();
-        // validateSync => object / error
-        // isValidSync => boolean
-        try {
-            this.setState({
-                error: null
-            })
-
-            SCHEMA.validateSync(this.state);
-        } catch (error) {
-            this.setState({
-                error
-            })
-        }
-    }
-
-    render() {
-        const { email, password, firstName, lastName, error } = this.state;
-        return (
-            <form onSubmit={this.submitHandler}>
-                <input type='text' name='firstName' placeholder='firstName' value={firstName} onChange={this.changeHandler} />
-                <input type='text' name='lastName' placeholder='lastName' value={lastName} onChange={this.changeHandler} />
-                <input type='text' name='email' placeholder='email' value={email} onChange={this.changeHandler} />
-                <input type='text' name='password' placeholder='password' value={password} onChange={this.changeHandler} />
-                <button type='submit'>Send form</button>
-                {error && <p className={styles.error}>{error.message}</p>}
-            </form>
-        );
-    }
+    return (
+        <Formik initialValues={initialState} onSubmit={handleSubmitToFormik} validationSchema={SCHEMA}>
+            {(formikProps) => {
+                return (
+                    <Form>
+                        <Field placeholder='firstName' name='firstName' />
+                        <ErrorMessage name='firstName'/>
+                        <Field placeholder='lastName' name='lastName' />
+                        <ErrorMessage name='lastName'/>
+                        <Field placeholder='email' name='email' />
+                        <ErrorMessage name='email'/>
+                        <Field type='password' placeholder='password' name='password' />
+                        <ErrorMessage name='password'/>
+                        <button type='submit'>Send</button>
+                    </Form>
+                )
+            }}
+        </Formik>
+    )
 }
+
 
 export default SignUpForm;
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 
 
 export const SCHEMA = yup.object({
-    firstName: yup.string().min(1).max(30),
-    lastName: yup.string().min(1).max(30),
-    email: yup.string().required().email(),
-    password: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+    firstName: yup.string('First name is a string!!!').required('Required!!!').min(1).max(30),
+    lastName: yup.string('Last name as a string!!!').required('Required!!!').min(1).max(30),
+    email: yup.string('Email as a string!!!').required('Email is required field!!!').email('Type valid email!!!'),
+    password: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 'Password is not valid!!!')
 })
